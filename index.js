608d3a6050b5cefcfea35800eb19933c85511deb
khushbookhator <khushbookhator11@gmail.com>
2021-07-21 10:27:40 +0530

    navlink functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".menu")
 const navOpen = document.querySelector(".hamburger")
 const navClose = document.querySelector(".close")
 const navBar = document.querySelector(".navbar")
-const navLink = document.querySelector(".nav-item")
+const navLink = document.querySelector(".nav-list")
 
 
 
@@ -17,23 +17,6 @@ navClose.addEventListener('click', () => {
 navLink.addEventListener('click', () => {
   menu.classList.remove("show")
 })
-
-// const navLeft = menu.getBoundingClientRect().left; 
-// navOpen.addEventListener("click", () => {
-//   if (navLeft < 0) {
-//     menu.classList.add("show");
-//     document.body.classList.add("show");
-//     navBar.classList.add("show");
-//   }
-// });
-
-// navClose.addEventListener("click", () => {
-//   if (navLeft < 0) {
-//     menu.classList.remove("show");
-//     document.body.classList.remove("show");
-//     navBar.classList.remove("show");
-//   }
-// });
 
 
 
